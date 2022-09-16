1f9568f4d67656746ef86aa6cac7003eb3eca19d
Mr. Ryan <60050719+ryanhhogan3@users.noreply.github.com>
2022-09-16 15:40:33 -0700

    Scatter Chart intro
    Need help creating a function that maps data to x and y cordinates on the scatter plot.

---
+++ investment-app/src/components/BarChartOptions.js
@@ -0,0 +1,21 @@
+//Displays all contents of Pie Chart
+export const BarChartOptions = {
+    plugins: {
+        title: {
+          display: true,
+          text: 'Chart.js Bar Chart - Stacked',
+        },
+      },
+      responsive: true,
+      scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          stacked: true,
+        },
+      },
+    };
+
+
+export default BarChartOptions
